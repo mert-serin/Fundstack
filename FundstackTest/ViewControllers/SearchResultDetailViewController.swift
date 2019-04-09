814d53fdfa10005c7b3e7ef489902f82ffef546a
mert-serin <mert@xotv.app>
2019-04-09 22:17:54 +0300

    SearchResultDetailViewController added

---
+++ FundstackTest/ViewControllers/SearchResultDetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SearchResultDetailViewController.swift
+//  FundstackTest
+//
+//  Created by Mert Serin on 9.04.2019.
+//  Copyright © 2019 Mert Serin. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultDetailViewController: UIViewController {
+
+    @IBOutlet weak var companyLogoImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
